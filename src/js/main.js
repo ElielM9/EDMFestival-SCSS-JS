0dc69b0c2d3a077d6d0b351952e82e3130f459c8
ElielM9 <elielm291@gmail.com>
2024-07-27 22:40:37 -0600

    Seccion galeria creada

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener(`DOMContentLoaded`, startApp);
 function startApp() {
   openCloseNav();
   colorHeaderScroll();
+  createGallery();
 }
 
 // Funciones
@@ -23,4 +24,34 @@ function colorHeaderScroll() {
 
     header.classList.toggle(`header--bg`, scrollPosY > 100);
   });
+}
+
+function createGallery() {
+  const galleryGrid = document.querySelector(`.gallery__grid`);
+
+  for (let i = 1; i <= 8; i++) {
+    const img = document.createElement(`picture`);
+    img.classList.add(`gallery__picture`);
+    img.innerHTML = `
+       <source srcset="./assets/img/gallery/${i}.avif" type="avif" />
+       <source srcset="./assets/img/gallery/${i}.webp" type="webp" />
+       <img 
+          class="gallery__img"
+          loading="lazy"
+          width="200"
+          height="300"
+          src="./assets/img/gallery/${i}.jpg"
+          alt="imagen-galeria"
+        />`;
+
+    img.addEventListener(`click`, showImg(i));
+
+    galleryGrid.appendChild(img);
+  }
+}
+
+
+
+function showImg(index) {
+  
 }
